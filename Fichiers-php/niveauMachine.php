91dd2c2550ca783745106a05a90748176c69fe78
Damien Lanusse <damienl.work@gmail.com>
2023-01-16 11:53:52 +0100

    Amélioration du formulaire interactif, ajout d'un php renvoyant auMain.html après traitement

---
+++ Fichiers-php/niveauMachine.php
@@ -0,0 +1,20 @@
+<?php
+    // On renvoit vers la page main.html
+    header("Refresh:0.1; url=../Main.html");
+
+    // Si on connait le niveau de la machine
+    if(isset($_POST['nivMachine'])){
+        // On récupère le niveau de la machine
+        $niveauMachine = $_POST['nivMachine'];
+
+        // On place le niveau de la machine dans une variable locale javascript
+        print "<script type='text/javascript'>localStorage.setItem('nivMachine', '$niveauMachine')</script>";
+    }
+    else{
+        // Sinon on place qu'il n'y a pas de machine dans une variable locale javascript
+        print "<script type='text/javascript'>localStorage.setItem('nivMachine', 'none')</script>";
+    }
+
+
+
+?>
